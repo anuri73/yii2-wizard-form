1885df82b9eec1f6fda22b68b510f9d162e1c927
Urmat Zhenaliev <sonkei@ya.ru>
2017-06-05 08:26:14 +0600

    Initial commit

---
+++ dist/Asset.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Urmat
+ * Date: 05.06.2017
+ * Time: 8:23
+ */
+
+namespace sonkei\wzard\dist;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class Asset
+ * @package sonkei\wzard\dist
+ */
+class Asset extends AssetBundle {
+
+}
---
+++ dist/Widget.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Urmat
+ * Date: 05.06.2017
+ * Time: 8:21
+ */
+
+namespace sonkei\wzard\dist;
+
+use yii\base\Widget as YiiWidget;
+
+/**
+ * Class Widget
+ * @package sonkei\wzard\dist
+ */
+class Widget extends YiiWidget {
+
+}
