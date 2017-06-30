d43b3599ba45c44a04d0b00e7e3e230f095b86c4
Urmat Zhenaliev <sonkei@ya.ru>
2017-07-01 01:56:51 +0600

    Asset

---
+++ dist/Asset.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Urmat
+ * Date: 01.07.2017
+ * Time: 1:54
+ */
+
+namespace anuri73\wzard;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class Asset
+ * @package anuri73\wzard
+ */
+class Asset extends AssetBundle {
+	/** @inheritdoc */
+	public $css = [];
+	/** @inheritdoc */
+	public $js = [];
+	/** @inheritdoc */
+	public $sourcePath = "@anuri73/wzard/assets";
+	/** @inheritdoc */
+	public $depends = [
+		'yii\web\YiiAsset',
+		'yii\bootstrap\BootstrapAsset',
+	];
+	/** @inheritdoc */
+	public $publishOptions = [
+		'forceCopy' => YII_DEBUG
+	];
+}
